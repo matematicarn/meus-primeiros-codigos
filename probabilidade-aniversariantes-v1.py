c76db6b9c64c4a5d85c94f891f61f5d73aaadb9b
Francisco Medeiros <64692574+matematicarn@users.noreply.github.com>
2020-05-05 13:03:59 -0300

    Update probabilidade-aniversariantes-v1.py
    O arredondamento, por meio de _round_ não estava correto. Pelo o que pesquisei, a recomendação é usar o módulo _decimal_, mas ainda não sei como fazê-lo. Portanto, fiz uma ajuste usando _format_, o que não é o ideal, mas resolve o erro.

--- probabilidade-aniversariantes-v1.py
+++ probabilidade-aniversariantes-v1.py
@@ -8,4 +8,4 @@ while r <= n:
     r += 1
 else:
     prob = (1 - math.prod(lista)/(365**n))*100
-print(f'\n Num grupo de {n} pessoas, a probabilidade de haver pelo menos duas pessoas que tenham o mesmo dia de aniversário é de {round(prob)}%.')
+print(f'\n Num grupo de {n} pessoas, a probabilidade de haver pelo menos duas pessoas que tenham o mesmo dia de aniversário é de {%.2f} %.' % round(prob,2)})
