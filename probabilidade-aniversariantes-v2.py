abb66bda457a8d5c219055572409e860b27e4b3a
Francisco Medeiros <64692574+matematicarn@users.noreply.github.com>
2020-05-05 13:18:13 -0300

    Update probabilidade-aniversariantes-v2.py
    Correção num problema de arredondamento.

--- probabilidade-aniversariantes-v2.py
+++ probabilidade-aniversariantes-v2.py
@@ -1,5 +1,5 @@
 # Em um grupo de n pessoas, calcule a probabilidade de haver pelo menos duas pessoas que façam aniversário no mesmo dia.
 import math
 n = int(input("Há quantas pessoas no grupo? "))
-prob = 100*(1 - math.perm(365,n)/365**n)
-print(f"\n Num grupo de {n} pessoas, a probabilidade de haver pelo menos duas pessoas que tenham o mesmo dia de aniversário é de {round(prob)}%.")
+prob = (1 - math.perm(365,n)/365**n)
+print(f"\n Num grupo de {n} pessoas, a probabilidade de haver pelo menos duas pessoas que tenham o mesmo dia de aniversário é de %.2f." % round(prob,2))
