c8280d9f167ec8b9e49661babdc687dd4341e53e
Francisco Medeiros <64692574+matematicarn@users.noreply.github.com>
2020-05-05 13:15:47 -0300

    Update probabilidade-aniversariantes-v1.py

--- probabilidade-aniversariantes-v1.py
+++ probabilidade-aniversariantes-v1.py
@@ -7,5 +7,5 @@ while r <= n:
     lista.append(366-r)
     r += 1
 else:
-    prob = (1 - math.prod(lista)/(365**n))*100
-print(f'\n Num grupo de {n} pessoas, a probabilidade de haver pelo menos duas pessoas que tenham o mesmo dia de aniversário é de {%.2f} %.' % round(prob,2)})
+    prob = (1 - math.prod(lista)/(365**n))
+print(f'\n Num grupo de {n} pessoas, a probabilidade de haver pelo menos duas pessoas que tenham o mesmo dia de aniversário é de %.2f.' % round(prob,2)})
